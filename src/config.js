f3970b695c54ccf3f5698bed70e83877b1158527
Imisuuu <mikolaj.zyszkiewicz1@gmail.com>
2022-01-20 15:50:01 +0100

    version 1.0.3💫

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: 'OTEzODY1MDc1MTgwMzE0NjY0.YaEtLA._0Yq1owBVj8Amyxk3iN585qgs14',
-    prefix: '$',
-    key: '7dAWajOtPQn7l2zr86VQxuLey8eeZaTiYOzR82QPnasVRwNQL1blqVkx19fHsO2S',
-    secret: 'I34VA1lb1tAqSkMbKhRQJFvJv0nIG5YLj53TbRhpfx2lXGKeeMjMzWp7vY3ImIGo',
-    playing: `Type $help`,
+    token: 'XXX',
+    prefix: 'X',
+    key: 'XXX',
+    secret: 'XXX',
+    playing: `XXX`,
 };
